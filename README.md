458ce744412216cd44f974750b5620d83dcb1dde
bean-frog <gkieran32855@gmail.com>
2023-10-03 00:49:45 -0700

    bye bye sveltekit it wasnt fun while it lasted

--- README.md
+++ README.md
@@ -1,38 +1,12 @@
-# create-svelte
+## Super simple task organizer for school or whatever
+most of this was made while tired so the code is terrible
+### features:
+- automatically saves in browser 
+- shows percent completion
+- custom icon/border color for each task
+- probably some other cool stuff
 
-Everything you need to build a Svelte project, powered by [`create-svelte`](https://github.com/sveltejs/kit/tree/master/packages/create-svelte).
-
-## Creating a project
-
-If you're seeing this, you've probably already done this step. Congrats!
-
-```bash
-# create a new project in the current directory
-npm create svelte@latest
-
-# create a new project in my-app
-npm create svelte@latest my-app
-```
-
-## Developing
-
-Once you've created a project and installed dependencies with `npm install` (or `pnpm install` or `yarn`), start a development server:
-
-```bash
-npm run dev
-
-# or start the server and open the app in a new browser tab
-npm run dev -- --open
-```
-
-## Building
-
-To create a production version of your app:
-
-```bash
-npm run build
-```
-
-You can preview the production build with `npm run preview`.
-
-> To deploy your app, you may need to install an [adapter](https://kit.svelte.dev/docs/adapters) for your target environment.
+### problems:
+- reloads page to fully clear tasks (hacky fix but it works)
+- atrocious amount of vertical spacing. i tried for ~45 min to fix but idk why its there still
+- no dark mode (yet)
---
+++ app.js
@@ -0,0 +1,147 @@
+// defs
+const iconSel = document.getElementById('icon-sel');
+const modal = document.getElementById('modal');
+const icons = ['fa-flag', 'fa-circle-exclamation', 'fa-star', 'fa-face-smile', 'fa-face-meh', 'fa-face-frown', 'fa-bolt-lightning', 'fa-heart', 'fa-poo', 'fa-bookmark', 'fa-fire', 'fa-money-bill'];
+const iconDisplay = document.getElementById('icon-display');
+const closeModal = document.getElementById('close-modal');
+const addTask = document.getElementById('add-task');
+const titleInput = document.getElementById('titleInput');
+const descInput = document.getElementById('descInput');
+const colorPicker = document.getElementById('colorPicker');
+const createTaskBtn = document.getElementById('createTaskBtn');
+const cardArea = document.getElementById('card-area');
+const totalTasks = document.getElementById('total-tasks');
+const completedTasks = document.getElementById('completed-tasks');
+const percentCompleted = document.getElementById('percent-completed');
+const clearTasksButton = document.getElementById('clear-all');
+let tasks = []
+if (localStorage.getItem('tasks'))  {
+tasks = JSON.parse(localStorage.getItem('tasks'))
+}
+
+// all onload functions 
+document.addEventListener('DOMContentLoaded', function() {
+    // icon sel init
+      for (let i = 0; i < icons.length; i++) {
+        let option = document.createElement('option');
+        let iconName = icons[i].replace('fa-', '');
+        option.value = icons[i];
+        option.textContent = iconName;
+        iconSel.appendChild(option);
+      }
+    iconDisplay.classList.add('fas', iconSel.value);
+    if (cardArea.childNodes.length <= 0) {
+        cardArea.innerHTML = `
+        <div class="hero min-h-screen bg-base-200">
+  <div class="hero-content text-center">
+    <div class="max-w-md">
+      <h1 class="text-5xl font-bold">You don't have any tasks!</h1>
+      <p class="py-6">Try adding some with the button below</p>
+      <button onclick="modal.classList.remove('invisible')"class="btn btn-primary">Add Task</button>
+    </div>
+  </div>
+</div>
+        `
+    } else {
+        return;
+    }
+  });
+
+
+// icon display
+iconSel.addEventListener('change', function() {
+    iconDisplay.setAttribute('class', '')
+iconDisplay.classList.add('fas', iconSel.value)
+});
+
+// open/close modal
+addTask.addEventListener('click', function() {
+    modal.classList.remove('invisible');
+})
+closeModal.addEventListener('click', function() {
+    modal.classList.add('invisible')
+})
+
+//task creation
+createTaskBtn.addEventListener('click', () => {
+    const title = titleInput.value.trim();
+    const desc = descInput.value.trim();
+    if (!title || !desc) {
+        alert('Please fill in all fields.');
+        return; 
+    }
+    const newTask = {
+        title: titleInput.value,
+        desc: descInput.value,
+        iconClass: iconSel.value,
+        cardBorderColor: colorPicker.value
+    };
+    tasks.push(newTask);
+    localStorage.setItem('tasks', JSON.stringify(tasks));
+    modal.classList.add('invisible')
+    renderCards();
+    updateStats();
+    titleInput.value = '';
+    descInput.value = '';
+});
+
+function renderCards() {
+    cardArea.innerHTML = '';
+    tasks.forEach(task => {
+        let cardTemplate = `
+        <div class="absolute left-2 top-2"><i class="text-3xl fas ${task.iconClass}"></i></div>
+                <div class="absolute right-2 top-2"><input type="checkbox" class="h-6 w-6 mr-4" id="checkbox"><button><i class="fas fa-trash-alt text-3xl text-red-600"></i></button></div>
+                <div class="mt-8">
+                     <h1 class="text-xl text-black">${task.title}</h1>
+                    <h1 class="text-black text-l overflow-hidden max-w-[400px]">${task.desc}</h1>
+                </div>
+        `
+      const cardDiv = document.createElement('div');
+      cardDiv.setAttribute('class', 'relative p-6 m-2 shadow-lg h-fit');
+      cardDiv.setAttribute('style', `width: 400px !important; border: 2px solid ${task.cardBorderColor}`)
+      cardDiv.innerHTML = cardTemplate
+    cardArea.appendChild(cardDiv);
+    });
+  }
+    renderCards();
+    window.addEventListener('storage', (event) => {
+    if (event.key === 'tasks') {
+      tasks = JSON.parse(localStorage.getItem('tasks')) || [];
+      renderCards();
+    }
+  });
+  function updateStats() {
+    amtTasks = tasks.length;
+  totalTasks.textContent = " " + amtTasks;
+    const checkboxes = document.querySelectorAll('input[type="checkbox"]');
+    let checkedCount = 0;
+    
+    function updateCompletedTasks() {
+      checkedCount = 0;
+      checkboxes.forEach((checkbox) => {
+        if (checkbox.checked) {
+          checkedCount++;
+        }
+      });
+      completedTasks.textContent = " " + checkedCount;
+      
+      const percentComplete = (checkedCount / amtTasks) * 100;
+      percentCompleted.textContent = " " + Math.round(percentComplete) + '%';
+    }
+    checkboxes.forEach((checkbox) => {
+      checkbox.addEventListener('change', updateCompletedTasks);
+    });
+    
+    updateCompletedTasks();
+  }
+  
+  updateStats();
+  function clearAllTasks() {
+    if (window.confirm("This action is irreversible. Continue?")) {
+        localStorage.clear('tasks');
+       location.reload();
+      } else {
+        window.alert("Okay, canceling task deletion");
+        return;
+      }
+  }
